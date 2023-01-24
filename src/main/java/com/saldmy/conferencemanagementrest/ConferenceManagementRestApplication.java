ba79e5d820f6fe220eb894e6cef1d06d015d40c3
Dmytro Salo <dmytro_salo1@epam.com>
2023-01-24 20:18:26 +0200

    Initial Spring Initializr commit

---
+++ src/main/java/com/saldmy/conferencemanagementrest/ConferenceManagementRestApplication.java
@@ -0,0 +1,13 @@
+package com.saldmy.conferencemanagementrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConferenceManagementRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConferenceManagementRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/saldmy/conferencemanagementrest/ConferenceManagementRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saldmy.conferencemanagementrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConferenceManagementRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
